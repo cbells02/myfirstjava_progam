ff75cc1652046f41c0effc910baf1d8d9655d433
Charles <cbells002@gmail.com>
2021-12-07 18:09:38 -0600

    this a calculator program

--- src/Hello.java
+++ src/Hello.java
@@ -1,4 +1,15 @@
 public class Hello {
+    String name;
+    int age;
+
+    public Hello(String name1, int ageNum){
+        age = ageNum;
+        name = name1;
+
+
+
+    }
+
 
     public static void main (String[] args){
 
@@ -6,5 +17,16 @@ public class Hello {
         System.out.println("Why is this so hard");
         System.out.println("I will be a programmer");
         System.out.println("this is gonna be good");
+
+        int age = 35;
+        System.out.println(age);
+
+        String name = "yo";
+        System.out.println(name);
+
+        Hello Student1 = new Hello("Charles", 55);
+        System.out.println(Student1.age);
+
+
     }
 }
